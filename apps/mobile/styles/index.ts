4eee269afada12cf401586ceeb559650e5bd8653
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2026-07-17 12:05:18 +0000

    feat: index - exporting styles

---
+++ apps/mobile/styles/index.ts
@@ -0,0 +1,11 @@
+import { typographyStyles as typography } from './typography';
+import { layoutStyles as layout } from './layout';
+import { componentStyles as components } from './components';
+
+export const globalStyles = {
+  typography,
+  layout,
+  components,
+};
+
+export default globalStyles;
